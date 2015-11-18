2b2ede59614647b79ea0e0a704f55ef79852c805
rifkyzulfikarf <rifkyzulfikar92@gmail.com>
2015-11-18 09:13:20 +0700

    laporan kas bank + piutang done

---
+++ modules/view/laporan/kas_bank.php
@@ -0,0 +1,154 @@
+<?php
+	$data = new koneksi();
+?>
+<section class="wrapper site-min-height">
+	<div class="row">
+		<div class="col-sm-12">
+			<section class="panel">
+				<header class="panel-heading">
+					Laporan Kas Bank
+					<span class="tools pull-right">
+						<button data-toggle="modal" href="#mdl-kriteria" class="btn btn-primary btn-sm"><i class="fa fa-tint"></i> Kriteria</button>
+					</span>
+				</header>
+				<div class="panel-body">
+					<?php  
+						if (isset($_POST['bank']) && $_POST['bank'] != "" && isset($_POST['tgl-awal']) && $_POST['tgl-awal'] != "" && 
+						isset($_POST['tgl-akhir']) && $_POST['tgl-akhir'] != "") {
+					?>
+						<div class="pull-right">
+							<button class="btn btn-default btn-sm" id="btn-print"><i class="fa fa-print"></i> Print</button>
+						</div>
+						<div id="print-section">
+							<div class="text-center"><h3>Laporan Kas Bank <?php echo ($_POST['bank'] == "1")?"PT. Energas Nusantara":"PT. Sumber Gasindo Jaya"; ?></h3></div><hr>
+							Periode : <?php echo $_POST['tgl-awal']." s/d ".$_POST['tgl-akhir'] ?><br><br>
+							<table class="table table-hover table-striped table-mod">
+								<thead>
+									<tr>
+										<th class="text-center">Tanggal</th>
+										<th class="text-center">No. Bukti</th>
+										<th class="text-center">Keterangan</th>
+										<th class="text-center">Jenis</th>
+										<th class="text-center">Setor</th>
+										<th class="text-center">Tarik</th>
+										<th class="text-center">Bea Admin</th>
+										<th class="text-center">Saldo</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+									$query = "SELECT `kas_bank`.`tgl`, `kas_bank`.`no_bukti`, `kas_bank`.`keterangan`, `kas_bank`.`jenis`, 
+									`kas_bank`.`setor`, `kas_bank`.`tarik`, `kas_bank`.`bea_admin`, `kas_bank`.`saldo` FROM `kas_bank` 
+									INNER JOIN `bank` ON (`kas_bank`.`id_bank` = `bank`.`id`)  
+									WHERE `kas_bank`.`id_bank` LIKE '".$_POST['bank']."' 
+									AND `kas_bank`.`tgl` BETWEEN '".$_POST['tgl-awal']."' AND '".$_POST['tgl-akhir']."';";
+									
+									$totalSetor = 0; $totalTarik = 0; $totalAdmin = 0; $totalSaldo = 0;
+									
+									if ($daftar = $data->runQuery($query)){
+										while( $rs = $daftar->fetch_assoc() ){
+											
+											$totalSetor += $rs['setor'];
+											$totalTarik += $rs['tarik'];
+											$totalAdmin += $rs['bea_admin'];
+											$totalSaldo = $rs['saldo'];
+											
+											echo "<tr>
+												<td class='text-center'>".$rs['tgl']."</td>
+												<td>".$rs['no_bukti']."</td>
+												<td>".$rs['keterangan']."</td>
+												<td class='text-center'>".$rs['jenis']."</td>
+												<td class='text-center'>".number_format($rs['setor'],0,",",".")."</td>
+												<td class='text-center'>".number_format($rs['tarik'],0,",",".")."</td>
+												<td class='text-center'>".number_format($rs['bea_admin'],0,",",".")."</td>
+												<td class='text-center'>".number_format($rs['saldo'],0,",",".")."</td>";
+											echo "</tr>";
+										}
+									}
+									?>
+										<tr>
+											<td colspan="4"><strong>Total</strong></td>
+											<td class="text-center"><?php echo number_format($totalSetor,0,",",".") ?></td>
+											<td class="text-center"><?php echo number_format($totalTarik,0,",",".") ?></td>
+											<td class="text-center"><?php echo number_format($totalAdmin,0,",",".") ?></td>
+											<td class="text-center"><?php echo number_format($totalSaldo,0,",",".") ?></td>
+										</tr>
+								</tbody>
+							</table>
+							<p class="text-muted small">Dicetak oleh <?php echo $_SESSION['en-nama'] ?>, pada <?php echo date("d M Y"); ?> </p>
+						</div>
+					<?php
+						} else {
+					?>
+						<div class="text-center text-muted">
+							<p style="font-size:120px;"><i class="fa fa-question-circle"></i></p>
+							<p>Silahkan isi kriteria laporan dengan meng-klik tombol kriteria</p>
+						</div>
+					<?php
+						}
+					?>
+				</div>
+			</section>
+		</div>
+	</div>
+</section>
+
+<form action="./" method="POST" role="form">
+	<div class="modal fade " id="mdl-kriteria" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Kriteria</h4>
+				</div>
+				<div class="modal-body">
+					<input type="hidden" id="no_spa" name="no_spa" value="<?php echo e_url('modules/view/laporan/kas_bank.php'); ?>">
+					<div class="form-group">
+						<select class="form-control" id="bank" name="bank">
+							<?php
+								if ($query = $data->runQuery("SELECT `bank`.`id`, `bank`.`nama` FROM `bank`;")) {
+									while ($rs = $query->fetch_array()) {
+										echo "<option value='".$rs['id']."'>".$rs['nama']."</option>";
+									}
+								}
+							?>
+						</select>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="tgl-awal" name="tgl-awal" placeholder="Tanggal Awal">
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="tgl-akhir" name="tgl-akhir" placeholder="Tanggal Akhir">
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button data-dismiss="modal" class="btn btn-default btn-close-modal" type="button">Close</button>
+					<button class="btn btn-primary" type="submit" id="btn-simpan-data">Cari Sesuai Kriteria Diatas</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+
+<script>
+$(document).ready(function(){
+	
+	$('#tgl-awal').datepicker({
+		format : "yyyy-mm-dd",
+		autoclose : true
+	});
+	$('#tgl-akhir').datepicker({
+		format : "yyyy-mm-dd",
+		autoclose : true
+	});
+	
+	$('#btn-print').click(function(ev){
+		ev.preventDefault();
+		$('#print-section').print({
+			globalStyles: true,
+			timeout: 250
+		});
+	});
+	
+});
+</script>
---
+++ modules/view/laporan/piutang.php
@@ -0,0 +1,158 @@
+<?php
+	$data = new koneksi();
+?>
+<section class="wrapper site-min-height">
+	<div class="row">
+		<div class="col-sm-12">
+			<section class="panel">
+				<header class="panel-heading">
+					Laporan Piutang
+					<span class="tools pull-right">
+						<button data-toggle="modal" href="#mdl-kriteria" class="btn btn-primary btn-sm"><i class="fa fa-tint"></i> Kriteria</button>
+					</span>
+				</header>
+				<div class="panel-body">
+					<?php  
+						if (isset($_POST['konsumen']) && $_POST['konsumen'] != "" && isset($_POST['tgl-awal']) && $_POST['tgl-awal'] != "" && 
+						isset($_POST['tgl-akhir']) && $_POST['tgl-akhir'] != "") {
+					?>
+						<div class="pull-right">
+							<button class="btn btn-default btn-sm" id="btn-print"><i class="fa fa-print"></i> Print</button>
+						</div>
+						<div id="print-section">
+							<div class="text-center"><h3>Laporan Piutang PT. Sumber Gasindo Jaya</h3></div><hr>
+							Periode : <?php echo $_POST['tgl-awal']." s/d ".$_POST['tgl-akhir'] ?><br><br>
+							<table class="table table-hover table-striped table-mod">
+								<thead>
+									<tr>
+										<th class="text-center">Tgl Trx</th>
+										<th class="text-center">Nota</th>
+										<th class="text-center">Konsumen</th>
+										<th class="text-center">Barang</th>
+										<th class="text-center">Jumlah</th>
+										<th class="text-center">Satuan</th>
+										<th class="text-center">Total</th>
+										<th class="text-center">Tgl Tempo</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php
+									$query = "SELECT `penjualan`.`tgl`, `penjualan`.`no_nota`, `konsumen`.`nama` AS `nama_konsumen`, 
+									`barang`.`nama` AS `nama_barang`, `penjualan`.`jml`, `penjualan`.`harga_jual`, `penjualan`.`total_jual`, 
+									`penjualan`.`tgl_tempo` FROM `penjualan` 
+									INNER JOIN `konsumen` ON (`penjualan`.`id_konsumen` = `konsumen`.`id`)  
+									INNER JOIN `barang` ON (`penjualan`.`id_barang` = `barang`.`id`)  
+									WHERE `penjualan`.`jenis` = '4' AND 
+									`penjualan`.`total_bayar` < `penjualan`.`total_jual` AND 
+									`penjualan`.`id_konsumen` LIKE '".$_POST['konsumen']."' AND 
+									`penjualan`.`tgl` BETWEEN '".$_POST['tgl-awal']."' AND '".$_POST['tgl-akhir']."';";
+									
+									$total = 0;
+									
+									if ($daftar = $data->runQuery($query)){
+										while( $rs = $daftar->fetch_assoc() ){
+											
+											$total += $rs['total_jual'];
+											
+											echo "<tr>
+												<td class='text-center'>".$rs['tgl']."</td>
+												<td>".$rs['no_nota']."</td>
+												<td>".$rs['nama_konsumen']."</td>
+												<td>".$rs['nama_barang']."</td>
+												<td class='text-center'>".number_format($rs['jml'],0,",",".")."</td>
+												<td class='text-center'>".number_format($rs['harga_jual'],0,",",".")."</td>
+												<td class='text-center'>".number_format($rs['total_jual'],0,",",".")."</td>
+												<td>".$rs['tgl_tempo']."</td>";
+											echo "</tr>";
+										}
+									}
+									?>
+										<tr>
+											<td colspan="6"><strong>Total</strong></td>
+											<td class="text-center"><?php echo number_format($total,0,",",".") ?></td>
+											<td></td>
+										</tr>
+								</tbody>
+							</table>
+							<p class="text-muted small">Dicetak oleh <?php echo $_SESSION['en-nama'] ?>, pada <?php echo date("d M Y"); ?> </p>
+						</div>
+					<?php
+						} else {
+					?>
+						<div class="text-center text-muted">
+							<p style="font-size:120px;"><i class="fa fa-question-circle"></i></p>
+							<p>Silahkan isi kriteria laporan dengan meng-klik tombol kriteria</p>
+						</div>
+					<?php
+						}
+					?>
+				</div>
+			</section>
+		</div>
+	</div>
+</section>
+
+<form action="./" method="POST" role="form">
+	<div class="modal fade " id="mdl-kriteria" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">Kriteria</h4>
+				</div>
+				<div class="modal-body">
+					<input type="hidden" id="no_spa" name="no_spa" value="<?php echo e_url('modules/view/laporan/piutang.php'); ?>">
+					<div class="form-group">
+						<select class="form-control" id="konsumen" name="konsumen">
+							<option value="%">Semua</option>
+							<?php
+								if ($query = $data->runQuery("SELECT `konsumen`.`id`, `konsumen`.`nama` FROM `konsumen`;")) {
+									while ($rs = $query->fetch_array()) {
+										echo "<option value='".$rs['id']."'>".$rs['nama']."</option>";
+									}
+								}
+							?>
+						</select>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="tgl-awal" name="tgl-awal" placeholder="Tanggal Awal">
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="tgl-akhir" name="tgl-akhir" placeholder="Tanggal Akhir">
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button data-dismiss="modal" class="btn btn-default btn-close-modal" type="button">Close</button>
+					<button class="btn btn-primary" type="submit" id="btn-simpan-data">Cari Sesuai Kriteria Diatas</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+
+<script>
+$(document).ready(function(){
+	
+	$('#tgl-awal').datepicker({
+		format : "yyyy-mm-dd",
+		autoclose : true
+	});
+	$('#tgl-akhir').datepicker({
+		format : "yyyy-mm-dd",
+		autoclose : true
+	});
+	
+	$('#btn-print').click(function(ev){
+		ev.preventDefault();
+		$('#print-section').print({
+			globalStyles: true,
+			timeout: 250
+		});
+	});
+	
+	$('#mdl-kriteria').on('shown.bs.modal', function () {
+		$('#konsumen', this).chosen();
+	});
+	
+});
+</script>
