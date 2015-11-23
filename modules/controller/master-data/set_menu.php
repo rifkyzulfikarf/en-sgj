6b66c2f2fd5fcbffdc3d515ebbd204ed05bf3a22
rifkyzulfikarf <rifkyzulfikar92@gmail.com>
2015-11-23 07:37:44 +0700

    set-menu enhancement

--- modules/controller/master-data/set_menu.php
+++ modules/controller/master-data/set_menu.php
@@ -6,15 +6,23 @@ if (isset($_POST['menu']) && $_POST['menu'] != "" &&  isset($_POST['id']) && $_P
 
 	$arr = array();
 	$arr = $_POST['menu'];
+	
+	$query = "";
 
 	//----- netralkan 
 	$delete = $data->runQuery("DELETE FROM `akses` WHERE `id_pemakai` = '".d_code($_POST['id'])."';");
 
 	foreach ($arr as $key => $value) {
-		$add = $data->add_menu($_POST['id'], $value );
+		//$add = $data->add_menu($_POST['id'], $value );
+		$query .= "INSERT INTO `akses` (`id_pemakai`,`id_menu`) VALUES ('".d_code($_POST['id'])."','".$value."');";
 	}
-
-	echo "Data tersimpan..!";
+	
+	if ($result = $data->runMultipleQueries($query)) {
+		echo "Data tersimpan..!";
+	} else {
+		echo "Gagal menyimpan..!";
+	}
+	
 
 }
 
