e60695bac8a31349012c4a78f8fe3d241dfe88d8
rifkyzulfikarf <rifkyzulfikar92@gmail.com>
2016-01-10 16:53:27 +0700

    update notifikasi

--- modules/controller/get-notif.php
+++ modules/controller/get-notif.php
@@ -6,24 +6,21 @@
 					<li>
 						<p class='yellow'>You have new notifications</p>
 					</li>";
-		$pangkalanBelumAmbil = 0;
+		$pangkalanBelumJual = 0;
+		$penjualanBelumAccGudang = 0;
 		
-		$query = "SELECT `konsumen`.`id`, `konsumen`.`nama` AS `nama_konsumen`, `kuota_penjualan`.`jml_alokasi` 
-		FROM `kuota_penjualan` 
-		INNER JOIN `konsumen` ON (`kuota_penjualan`.`id_konsumen` = `konsumen`.`id`) 
-		WHERE tgl = '".date("Y-m-d")."';";
+		$query = "SELECT `id` FROM `konsumen` WHERE `pangkalan` = '1' AND `hapus` = '0';";
 		if ($result = $data->runQuery($query)) {
 			while ($rs = $result->fetch_array()) {
 				
-				$qCek = "SELECT SUM(`jml`) FROM `penjualan` WHERE `id_konsumen` = '".$rs["id"]."' AND 
-				`tgl` = '".date("Y-m-d")."' AND 
-				`id_barang` = '1';";
+				$qCek = "SELECT COUNT(`id`) FROM `penjualan` WHERE `id_konsumen` = '".$rs["id"]."' AND 
+				`tgl` = '".date("Y-m-d")."';";
 				if ($resCek = $data->runQuery($qCek)) {
 					$rsCek = $resCek->fetch_array();
-					$jumlahTerambil = $rsCek[0];
+					$jumlahPenjualan = $rsCek[0];
 					
-					if ($jumlahTerambil < $rs['jml_alokasi']) {
-						$pangkalanBelumAmbil += 1;
+					if ($jumlahPenjualan == 0) {
+						$pangkalanBelumJual += 1;
 					}
 					
 				}
@@ -31,7 +28,18 @@
 			}
 		}
 		
-		$listNotif .= "<li><a href='#'>".$pangkalanBelumAmbil." Pangkalan belum memenuhi alokasi.</a></li>";
+		$listNotif .= "<li><a href='#'>".$pangkalanBelumJual." Pangkalan belum melakukan<br>transaksi pada hari ini.</a></li>";
+		
+		$query = "SELECT COUNT(`penjualan_acc_gudang`.`id_penjualan`) FROM `penjualan_acc_gudang` INNER JOIN `penjualan` 
+				ON(`penjualan_acc_gudang`.`id_penjualan` = `penjualan`.`id`) WHERE `penjualan_acc_gudang`.`acc_gudang` IS NULL AND 
+				`penjualan_acc_gudang`.`id_gudang` IS NULL AND `penjualan`.`tgl` = '".date("Y-m-d")."';";
+		if ($result = $data->runQuery($query)) {
+			while ($rs = $result->fetch_array()) {
+					$penjualanBelumAccGudang = $rs[0];
+			}
+		}
+		
+		$listNotif .= "<li><a href='#'>".$penjualanBelumAccGudang." Penjualan belum diacc oleh gudang<br>pada hari ini.</a></li>";
 		
 		$arr['listNotif'] = $listNotif;
 		
